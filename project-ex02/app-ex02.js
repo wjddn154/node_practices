84c3d581d212e3f7749151e501652fed380fb0ee
wjddn154 <gjwjddn154@gmail.com>
2021-11-15 12:26:44 +0900

    first commit

---
+++ project-ex02/app-ex02.js
@@ -0,0 +1,23 @@
+/*
+douzone-bipa-math npm 모듈 테스트(모듈 패키지: 로컬 배포)
+
+
+$ npm i ../douzone.wjddn154-mat
+명령으로 설치 후 테스트 할 것
+*/
+
+
+var dzMath = require('douzone-wjddn154-math');
+
+console.log(dzMath.sum(10, 20, 30, 40, 50));
+console.log(dzMath.max(10, 20, 30, 40, 50));
+console.log(dzMath.min(10, 20, 30, 40, 50));
+
+
+
+
+
+
+
+
+
